3187cb2cc418be4a0e0b773939723eb2141ce95f
Ignacio Royo-Villanova <royovillanova.i@gmail.com>
2023-12-08 19:51:29 +0100

    add draft status

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,7 +12,8 @@ const postsCollection = defineCollection({
       url: z.string(),
       alt: z.string()
     }),
-    tags: z.array(z.string())
+    tags: z.array(z.string()),
+    draft: z.boolean().optional(),
   })
 });
 // Exporta un solo objeto `collections` con las colecciones registradas
--- src/content/posts/git-trabajando-con-ramas.md
+++ src/content/posts/git-trabajando-con-ramas.md
@@ -7,6 +7,7 @@ image:
     url: "https://images.unsplash.com/photo-1618401479427-c8ef9465fbe1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2043&q=80"
     alt: 'git'
 tags: [git, web]
+
 ---
 
 
